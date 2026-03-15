b88ec9274b59b624544ca18118e901ce1fc4c977
Christoph Cullmann <christoph@cullmann.dev>
2026-03-15 15:57:18 +0100

    fix reading of spellchecking element to match XSD
    did before just work by accident as the general reading
    was broken and did not properly end at </general>
    
    remove ignored, that is implied if no char="x" is given
    
    fix that if no char is given, we now map the sequence to the null char
    
    before all ignored stuff was just without any effect, as KTextEditor wanted
    a mapping to null
    
    document that
    
    add test for the null char mapping that was missing before
    
    BUG: 517428

--- src/lib/definition.cpp
+++ src/lib/definition.cpp
@@ -360,6 +360,10 @@ bool DefinitionData::load(OnlyKeywords onlyKeywords)
         else if (reader.name() == QLatin1String("general")) {
             loadGeneral(reader);
         }
+
+        else if (reader.name() == QLatin1String("spellchecking")) {
+            loadSpellchecking(reader);
+        }
     }
 
     for (auto &kw : keywordLists) {
@@ -662,14 +666,9 @@ void DefinitionData::loadGeneral(QXmlStreamReader &reader)
     Q_ASSERT(reader.tokenType() == QXmlStreamReader::StartElement);
     reader.readNext();
 
-    // reference counter to count XML child elements, to not return too early
-    int elementRefCounter = 1;
-
     while (!reader.atEnd()) {
         switch (reader.tokenType()) {
         case QXmlStreamReader::StartElement:
-            ++elementRefCounter;
-
             if (reader.name() == QLatin1String("keywords")) {
                 if (reader.attributes().hasAttribute(QLatin1String("casesensitive"))) {
                     caseSensitive = Xml::attrToBool(reader.attributes().value(QLatin1String("casesensitive"))) ? Qt::CaseSensitive : Qt::CaseInsensitive;
@@ -694,16 +693,13 @@ void DefinitionData::loadGeneral(QXmlStreamReader &reader)
                 loadFoldingIgnoreList(reader);
             } else if (reader.name() == QLatin1String("comments")) {
                 loadComments(reader);
-            } else if (reader.name() == QLatin1String("spellchecking")) {
-                loadSpellchecking(reader);
             } else {
                 reader.skipCurrentElement();
             }
             reader.readNext();
             break;
         case QXmlStreamReader::EndElement:
-            --elementRefCounter;
-            if (elementRefCounter == 0) {
+            if (reader.name() == QLatin1String("general")) {
                 return;
             }
             reader.readNext();
@@ -801,11 +797,12 @@ void DefinitionData::loadSpellchecking(QXmlStreamReader &reader)
         case QXmlStreamReader::StartElement:
             ++elementRefCounter;
             if (reader.name() == QLatin1String("encoding")) {
+                // target character, if not given map to null char to ignore during spell checking
                 const auto charRef = reader.attributes().value(QLatin1String("char"));
-                if (!charRef.isEmpty()) {
-                    const auto str = reader.attributes().value(QLatin1String("string"));
-                    characterEncodings.push_back({charRef[0], str.toString()});
-                }
+                const auto target = charRef.isEmpty() ? QChar() : charRef.first();
+
+                const auto str = reader.attributes().value(QLatin1String("string"));
+                characterEncodings.push_back({target, str.toString()});
             }
             reader.readNext();
             break;
